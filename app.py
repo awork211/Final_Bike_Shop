106fe00e1205d548dded6c791cf16f4e90d42792
awork211 <work.aaron89@gmail.com>
2022-05-03 08:21:44 -0400

    added routes

--- app.py
+++ app.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template('Main-Page.html')
 
+@app.route("/registration")
+def register():
+    return render_template('Registration.html')
+
+@app.route("/login")
+def login():
+    return render_template('Login.html')
 
 if __name__ == '__main__':
     app.run(Debug=True)
