f72595985e1cef21de1f328b276f6b7ec39e958d
JusticeKipp <jfkipp211@stevenscollege.edu>
2022-05-03 10:02:50 -0400

    did a thing

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
+@app.route("/Login")
+def login():
+    return render_template('Login.html')
+
 @app.route("/")
 def index():
     return render_template('Main-Page.html')
@@ -9,10 +13,6 @@ def index():
 @app.route("/register")
 def register():
     return render_template('Registration.html')
-
-@app.route("/login")
-def login():
-    return render_template('Login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
