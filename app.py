6388edd1a2dc37592fcfa1e2a19f5721b3b40c1f
awork211 <work.aaron89@gmail.com>
2022-05-04 09:25:57 -0400

    form vars, crypto objects, pem files

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import psycopg2
 from flask import Flask, render_template, request, url_for, redirect
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from binascii import hexlify
 
 app = Flask(__name__)
 
@@ -22,6 +25,16 @@ cur = conn.cursor()
 # """)
 # conn.commit()
 
+pr_key = RSA.import_key(open('private.pem', 'r').read())
+pu_key = RSA.import_key(open('public.pem', 'r').read())
+
+# encrypt message
+cipher = PKCS1_OAEP.new(key=pu_key)
+# cipher_text = cipher.encrypt(message)
+
+# decrypt message
+decrypt = PKCS1_OAEP.new(key=pr_key)
+# decrypted_message = decrypt.decrypt(cipher_text)
 @app.route("/")
 def index():
     return render_template('Main-Page.html')
@@ -29,11 +42,22 @@ def index():
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        email = request.form['email']
         user = request.form['user']
-        password = request.form['pasw']
+        # password = request.form['pasw']
+        email = request.form['email']
         phone = request.form['phone']
         address = request.form['address']
+        print(email, user, phone, address)
+        # insert into db table
+        sql = """
+            INSERT INTO user_info (email, username, phone, address)
+            VALUES (%s, %s, %s, %s)
+        """
+        args = email, user, phone, address
+
+        cur.execute(sql, args)
+        conn.commit()
+
     return render_template('Registration.html')
 
 @app.route("/login")
