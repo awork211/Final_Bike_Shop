8e7b443ab422d1aa164a5053774579e2a9769902
JusticeKipp <jfkipp211@stevenscollege.edu>
2022-05-05 08:46:18 -0400

    fixed background image on login page

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ from binascii import hexlify
 app = Flask(__name__)
 
 # connect database
-conn = psycopg2.connect(
-    host = 'localhost',
-    database = 'user_info'
-)
+# conn = psycopg2.connect(
+#     host = 'localhost',
+#     database = 'user_info'
+# )
 
-cur = conn.cursor()
+# cur = conn.cursor()
 
 # cur.execute("""
 # CREATE TABLE user_info (
@@ -25,11 +25,11 @@ cur = conn.cursor()
 # """)
 # conn.commit()
 
-pr_key = RSA.import_key(open('private.pem', 'r').read())
-pu_key = RSA.import_key(open('public.pem', 'r').read())
+# pr_key = RSA.import_key(open('private.pem', 'r').read())
+# pu_key = RSA.import_key(open('public.pem', 'r').read())
 
 # encrypt obj
-cipher = PKCS1_OAEP.new(key=pu_key)
+# cipher = PKCS1_OAEP.new(key=pu_key)
 
 # decrypt message
 # decrypted_message = decrypt.decrypt(cipher_text)
@@ -37,37 +37,37 @@ cipher = PKCS1_OAEP.new(key=pu_key)
 def index():
     return render_template('Login.html')
 
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        user = request.form['user']
-        password = request.form['pasw']
-        email = request.form['email']
-        phone = request.form['phone']
-        address = request.form['address']
+# @app.route("/register", methods=['GET', 'POST'])
+# def register():
+#     if request.method == 'POST':
+#         user = request.form['user']
+#         password = request.form['pasw']
+#         email = request.form['email']
+#         phone = request.form['phone']
+#         address = request.form['address']
 
-        enc_pass = cipher.encrypt(bytes(password, 'utf-8'))
-        print(email, user, phone, address, enc_pass)
-        # insert into db table
-        sql = """
-            INSERT INTO user_info (email, username, password, phone, address)
-            VALUES (%s, %s, %s, %s, %s)
-        """
-        args = email, user, enc_pass, phone, address
+#         enc_pass = cipher.encrypt(bytes(password, 'utf-8'))
+#         print(email, user, phone, address, enc_pass)
+#         # insert into db table
+#         sql = """
+#             INSERT INTO user_info (email, username, password, phone, address)
+#             VALUES (%s, %s, %s, %s, %s)
+#         """
+#         args = email, user, enc_pass, phone, address
 
-        cur.execute(sql, args)
-        conn.commit()
+#         cur.execute(sql, args)
+#         conn.commit()
 
-    return render_template('Registration.html')
+#     return render_template('Registration.html')
 
-@app.route("/login")
-def login():
-    # decrypt = PKCS1_OAEP.new(key=pr_key)
-    return render_template('Login.html')
+# @app.route("/login")
+# def login():
+#     # decrypt = PKCS1_OAEP.new(key=pr_key)
+#     return render_template('Login.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-cur.close()
-conn.close()
+# cur.close()
+# conn.close()
