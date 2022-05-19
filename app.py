024a753ef702d0fddf4faa79aa06e7ca14712e27
awork211 <work.aaron89@gmail.com>
2022-05-19 09:04:34 -0400

    item cart update

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from math import prod
 import psycopg2
+import psycopg2.extras
 from flask import Flask, render_template, request, url_for, redirect, session
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
@@ -16,6 +18,7 @@ conn = psycopg2.connect(
 
 cur = conn.cursor()
 
+# add users table
 # cur.execute("""
 # CREATE TABLE user_info (
 #    email varchar(200),
@@ -36,18 +39,135 @@ pu_key = RSA.import_key(open('public.pem', 'r').read())
 cipher = PKCS1_OAEP.new(key=pu_key)
 decrypt = PKCS1_OAEP.new(key=pr_key)
                                                                                                                                         
+# add products table
+# cur.execute("""
+#     CREATE TABLE products (
+#         name varchar(100),
+#         image varchar(100),
+#         category varchar(50),
+#         price numeric,
+#         quantity int
+#     )
+# """)
+# conn.commit()
+
+
 
 # decrypt message
-@app.route("/")
+@app.route("/", methods=["GET","POST"])
 def index():
     # products query here
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute("""
+        SELECT * FROM products WHERE category = 'complete'
+    """)
+    conn.commit()
+    products = cur.fetchall()
+        
+    
     if "user" in session:
         user = session['user']
         print(session)
-        return render_template('Main-Page.html', user = user)
+        return render_template('Main-Page.html', user = user, products = products)
     else:
         print(session)
-        return render_template('Main-Page.html')
+        return render_template('Main-Page.html', products = products)
+
+@app.route('/cart', methods=["GET","POST"])
+def addToCart():
+    if request.method == 'POST':
+        quantity = request.form['quantity']
+        itemName = request.form['itemName']
+        
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+        cur.execute("""
+            SELECT * FROM products WHERE name= %s
+        """, [itemName])
+        conn.commit()
+        product = cur.fetchone()
+
+        # creating item for cart
+        itemArray = { product['name']: {'quantity': quantity, 'price': product['price'], 'image': product['image'], 'total_price': product['price'] * int(quantity)}}
+        print(itemArray, 'printed')
+        cart_total_quantity = 0
+        cart_total_price = 0
+
+        # merge items in cart
+        def mergeArr(firstArr, secArr):
+            if isinstance(firstArr, list) and isinstance(secArr, list):
+                return firstArr + secArr
+            elif isinstance(firstArr, dict) and isinstance(secArr, dict):
+                return dict(list(firstArr.items()) + list(secArr.items()))
+            elif isinstance(firstArr, set) and isinstance(secArr, set):
+                return firstArr.union(secArr)
+            False
+        
+        if 'item_cart' in session:
+            print('in session')
+            # print(session['item_cart'][product['name']])
+            if product['name'] in session['item_cart']:
+                # add cart properties
+                for key, value in session['item_cart'].items():
+                    if product['name'] == key:
+                        print(session['item_cart'][key])
+                        old_quantity = session['item_cart'][key]['quantity']
+                        total_quantity = int(old_quantity) + int(quantity)
+                        session['item_cart'][key]['quantity'] = total_quantity
+                        session['item_cart'][key]['total_price'] = total_quantity * product['price']
+            else:
+                # merge new item with cart
+                session['item_cart'] = mergeArr(session['item_cart'], itemArray)
+            # add individual properties
+            for key, value in session['item_cart'].items():
+                individual_quantity = int(session['item_cart'][key]['quantity'])
+                individual_price = float(session['item_cart'][key]['total_price'])
+                cart_total_quantity = cart_total_quantity + individual_quantity
+                cart_total_price = cart_total_price + individual_price
+        else:
+            # add item in cart, add totals
+            session['item_cart'] = itemArray
+            cart_total_quantity = cart_total_quantity + int(quantity)
+            cart_total_price = cart_total_price + int(quantity) * product['price']
+        # set totals in session
+        session['cart_total_price'] = cart_total_price
+        session['cart_total_quantity'] = cart_total_quantity
+        
+        return render_template('cart.html')
+    elif request.method == 'GET':
+        return render_template('cart.html')
+    else:
+        return 'Error adding item to cart'
+
+@app.route('/clear')
+def clear():
+    session.clear()
+    return redirect(url_for('index'))
+
+@app.route('/delete/<string:name>')
+def delete_item(name):
+    cart_total_price = 0
+    cart_total_quantity = 0
+
+    # casted as list to avoid runtime err
+    for item in list(session['item_cart'].items()):
+        if name == item[0]:
+            # delete item from session
+            session['item_cart'].pop(name, None)
+            if 'item_cart' in session:
+                for key, value in session['item_cart'].items():
+                    individual_quantity = int(session['item_cart'][key]['quantity'])
+                    individual_price = float(session['item_cart'][key]['total_price'])
+                    cart_total_quantity = cart_total_quantity + individual_quantity
+                    cart_total_price = cart_total_price + individual_price
+    
+    # update cart total if not empty
+    if cart_total_quantity == 0:
+        session.pop('item_cart', None)
+    else:
+        session['cart_total_quantity'] = cart_total_quantity
+        session['cart_total_price'] = cart_total_price
+    return redirect(url_for('index')) #change url to cart
 
 @app.route("/purchase")
 def purchase():
@@ -135,6 +255,10 @@ def regMessage(message):
 def logout():
     session.pop("user", None)
     return redirect(url_for('login'))
+
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
 
 
 if __name__ == '__main__':
